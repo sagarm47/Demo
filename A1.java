b717c48ec025c60e80f8fdb65f9d587aa045bb4c
sagarm47 <59823365+sagarm47@users.noreply.github.com>
2020-02-25 17:48:16 +0530

    Add files via upload

---
+++ A1.java
@@ -0,0 +1,11 @@
+public class A1{
+	public static void main (String[] args){
+		for(int i=1; i<=10; i++){
+			if(i%2==0){
+			System.out.print("\n even numbers:"+i+" ");
+			}
+			else
+				System.out.print("\n odd numbers"+i);
+		}
+	}
+}
